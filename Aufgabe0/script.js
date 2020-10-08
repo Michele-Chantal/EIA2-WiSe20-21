cd9f921ac179067628de2299baba23b7d7596665
Michele-Chantal <64040092+Michele-Chantal@users.noreply.github.com>
2020-10-08 12:36:40 +0200

    Aufgabe 0

---
+++ Aufgabe0/script.js
@@ -0,0 +1,7 @@
+"use strict";
+var person = prompt("Please enter your name");
+if (person != null) {
+    document.getElementById("demo").innerHTML =
+        "Hello " + person + "! How are you today? :)";
+}
+//# sourceMappingURL=script.js.map
