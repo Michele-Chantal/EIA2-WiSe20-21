128d692bdc35c654a171d7cbface7e76e0ef4ef7
Michele-Chantal <64040092+Michele-Chantal@users.noreply.github.com>
2020-11-19 12:41:14 +0100

    Update Aufgabe 5

--- Aufgabe5/script.ts
+++ Aufgabe5/script.ts
@@ -1,26 +1,73 @@
 namespace L05_Hexenkessel_Client {
     window.addEventListener("load", handleLoad);
     let price: number = 0;
+    let formDataSendInstructions: FormData = new FormData();
+    let formDataSendGeneral: FormData = new FormData();
 
-    function handleLoad(_event: Event): void {
+    async function handleLoad(_event: Event): Promise<void> {
+        let response: Response = await fetch("newData.json");
+        let item: string = await response.text();
+        let data: Data = JSON.parse(item);
+        
         generateContent(data);
 
-        let anweisungen: HTMLDivElement = <HTMLDivElement>document.querySelector("div#anweisungen");
+        let send: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#btnSend");
+        send.addEventListener("click", sendRecipe);
+
         let button1: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#button1");
         let btnIngredients: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#btnIngredients");
         let btnTemp: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#btnTemp");
         let btnStir: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#btnStir");
+        let btnDelete: HTMLButtonElement = <HTMLButtonElement>document.querySelector("#delete");
 
         button1.addEventListener("click", displayInfos);
         btnIngredients.addEventListener("click", displayIngredients);
         btnTemp.addEventListener("click", displayTemp);
         btnStir.addEventListener("click", displayStir);
-        anweisungen.addEventListener("change", handleChange);
+        btnDelete.addEventListener("click", askBeforeDelete);
+        
     }
 
-    function handleChange(_event: Event): void {
-        displayInfos();
+    async function sendRecipe(): Promise<void> {
+        let outputGeneral: HTMLDivElement = <HTMLDivElement>document.querySelector("div#outputGeneral");
+        let outputInstructions: HTMLDivElement = <HTMLDivElement>document.querySelector("div#outputInstructions");
+        
+        if (outputGeneral.innerHTML && outputInstructions.innerHTML == "") {
+            alert("Professor Snape wird sich nicht freuen, wenn du eine leere Seite abgibst...");
+        } else {
+            console.log("Send Recipe");
+            let querySendGeneral: URLSearchParams = new URLSearchParams(<any>formDataSendGeneral);
+            let querySendInstructions: URLSearchParams = new URLSearchParams(<any>formDataSendInstructions);
+            await fetch("index.html?" + querySendGeneral.toString());
+            await fetch("index.html?" + querySendInstructions.toString());
+
+            //When recipe has been sent by user, delete all contents of output and all keys, values of formDataSends
+            deleteAll();
+            alert("Rezept gesendet!");
+        }
     }
+
+    function askBeforeDelete(): void {
+        let outputGeneral: HTMLDivElement = <HTMLDivElement>document.querySelector("div#outputGeneral");
+        let outputInstructions: HTMLDivElement = <HTMLDivElement>document.querySelector("div#outputInstructions");
+        if (outputGeneral.innerHTML && outputInstructions.innerHTML != "" && confirm("Bist du sicher, dass du alles löschen möchtest?")) {
+            deleteAll();
+        }
+    }
+
+    function deleteAll(): void {
+        let outputGeneral: HTMLDivElement = <HTMLDivElement>document.querySelector("div#outputGeneral");
+        let outputInstructions: HTMLDivElement = <HTMLDivElement>document.querySelector("div#outputInstructions");
+        let totalSpan: HTMLSpanElement = <HTMLSpanElement>document.querySelector("#totalSpan");
+        outputGeneral.innerHTML = "...";
+        outputInstructions.innerHTML = "";
+        totalSpan.innerHTML = "";
+
+        formDataSendGeneral = new FormData();
+        formDataSendInstructions = new FormData();
+    }
+
+    
 
     function displayInfos(): void {
         console.log("show info");
@@ -28,13 +75,14 @@ namespace L05_Hexenkessel_Client {
         let outputInfos: HTMLDivElement = <HTMLDivElement>document.querySelector("div#outputInfos");
         outputInfos.innerHTML = "";
 
-        let formData: FormData = new FormData(<HTMLFormElement>document.querySelector("form"));
+        let formDataGeneral: FormData = new FormData(<HTMLFormElement>document.querySelector("form"));
 
-        for (let entry of formData) {
+        for (let entry of formDataGeneral) {
             switch (entry[0]) {
                 case "Name":
                     if (entry[1] != "") {
                         outputInfos.innerHTML += "Name des Tranks: " + entry[1] + "<br>";
+                        formDataSendGeneral.append(entry[0], entry[1]);
                         console.log("case1");
                     }
                     break;
@@ -42,6 +90,7 @@ namespace L05_Hexenkessel_Client {
                 case "Beschreibung":
                     if (entry[1] != "") {
                         outputInfos.innerHTML += "Beschreibung: " + entry[1] + "<br>";
+                        formDataSendGeneral.append(entry[0], entry[1]);
                         console.log("case2");
                     }
                     break;
@@ -50,6 +99,7 @@ namespace L05_Hexenkessel_Client {
                     if (entry[1] != "-") {
                         outputInfos.innerHTML += "Trankwirkung: " + entry[1] + "<br>";
                         effect = true;
+                        formDataSendGeneral.append(entry[0], entry[1]);
                         console.log("case3");
                     }
                     break;
@@ -58,6 +108,7 @@ namespace L05_Hexenkessel_Client {
                     if (entry[1] != "0" && effect) {
                         outputInfos.innerHTML += "Dauer: " + entry[1] + "<br>";
                         console.log("case4");
+                        formDataSendGeneral.append(entry[0], entry[1]);
                         break;
                     }
 
@@ -76,20 +127,23 @@ namespace L05_Hexenkessel_Client {
         outputInstructions.innerHTML = "";
         totalPrice.innerHTML = "";
 
-        let formData2: FormData = new FormData(<HTMLFormElement>document.querySelector("form"));
+        let formDataInstructions: FormData = new FormData(<HTMLFormElement>document.querySelector("formInstructions"));
 
-        for (let entry of formData2) {
+        for (let entry of formDataInstructions) {
             if (entry[0] == "Ingredienzen") {
                 let selector: string = "[value='" + entry[1] + "']";
                 let item: HTMLInputElement = <HTMLInputElement>document.querySelector(selector);
 
                 let chosenAmount: string = entry[1] + "Menge";
-                let amount: number = Number(formData2.get(chosenAmount));
+                let amount: number = Number(formDataInstructions.get(chosenAmount));
 
                 let priceItem: number = Number(item.getAttribute("price"));
                 price += amount * priceItem;
 
                 outputInstructions.innerHTML += amount + " " + entry[1] + " hinzugeben." + "<br>";
+            
+                formDataSendInstructions.append(entry[0], entry[1]);
+                formDataSendInstructions.append(chosenAmount, amount.toString());
             }
         }
         outputInstructions.innerHTML += "<br>";
@@ -99,9 +153,10 @@ namespace L05_Hexenkessel_Client {
 
     function displayTemp (): void {
         let outputInstructions: HTMLDivElement = <HTMLDivElement>document.querySelector("div#outputInstructions");
-        let formData2: FormData = new FormData(<HTMLFormElement>document.querySelector("form"));
+        
+        let formDataInstructions: FormData = new FormData(<HTMLFormElement>document.querySelector("formInstructions"));
 
-        for (let entry of formData2) {
+        for (let entry of formDataInstructions) {
             switch (entry[0]) {
                 case "Temperatur":
                     outputInstructions.innerHTML += "Grad: " + entry[1];
