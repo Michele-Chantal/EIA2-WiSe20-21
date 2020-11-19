2add685ef21b12dbdf61615cc8158d6319de33fc
Michele-Chantal <64040092+Michele-Chantal@users.noreply.github.com>
2020-11-19 13:34:01 +0100

    Aufgabe 5 Update

--- Aufgabe5/script.js
+++ Aufgabe5/script.js
@@ -3,39 +3,79 @@ var L05_Hexenkessel_Client;
 (function (L05_Hexenkessel_Client) {
     window.addEventListener("load", handleLoad);
     let price = 0;
-    function handleLoad(_event) {
-        L05_Hexenkessel_Client.generateContent(L05_Hexenkessel_Client.data);
-        let anweisungen = document.querySelector("div#anweisungen");
+    let formDataSendInstructions = new FormData();
+    let formDataSendGeneral = new FormData();
+    async function handleLoad(_event) {
+        let response = await fetch("newData.json");
+        let item = await response.text();
+        let data = JSON.parse(item);
+        L05_Hexenkessel_Client.generateContent(data);
+        let send = document.querySelector("#btnSend");
+        send.addEventListener("click", sendRecipe);
         let button1 = document.querySelector("#button1");
         let btnIngredients = document.querySelector("#btnIngredients");
         let btnTemp = document.querySelector("#btnTemp");
         let btnStir = document.querySelector("#btnStir");
+        let btnDelete = document.querySelector("#delete");
         button1.addEventListener("click", displayInfos);
         btnIngredients.addEventListener("click", displayIngredients);
         btnTemp.addEventListener("click", displayTemp);
         btnStir.addEventListener("click", displayStir);
-        anweisungen.addEventListener("change", handleChange);
+        btnDelete.addEventListener("click", askBeforeDelete);
     }
-    function handleChange(_event) {
-        displayInfos();
+    async function sendRecipe() {
+        let outputGeneral = document.querySelector("div#outputGeneral");
+        let outputInstructions = document.querySelector("div#outputInstructions");
+        if (outputGeneral.innerHTML && outputInstructions.innerHTML == "") {
+            alert("Professor Snape wird sich nicht freuen, wenn du eine leere Seite abgibst...");
+        }
+        else {
+            console.log("Send Recipe");
+            let querySendGeneral = new URLSearchParams(formDataSendGeneral);
+            let querySendInstructions = new URLSearchParams(formDataSendInstructions);
+            await fetch("index.html?" + querySendGeneral.toString());
+            await fetch("index.html?" + querySendInstructions.toString());
+            //When recipe has been sent by user, delete all contents of output and all keys, values of formDataSends
+            deleteAll();
+            alert("Rezept gesendet!");
+        }
+    }
+    function askBeforeDelete() {
+        let outputGeneral = document.querySelector("div#outputGeneral");
+        let outputInstructions = document.querySelector("div#outputInstructions");
+        if (outputGeneral.innerHTML && outputInstructions.innerHTML != "" && confirm("Bist du sicher, dass du alles löschen möchtest?")) {
+            deleteAll();
+        }
+    }
+    function deleteAll() {
+        let outputGeneral = document.querySelector("div#outputGeneral");
+        let outputInstructions = document.querySelector("div#outputInstructions");
+        let totalSpan = document.querySelector("#totalSpan");
+        outputGeneral.innerHTML = "...";
+        outputInstructions.innerHTML = "";
+        totalSpan.innerHTML = "";
+        formDataSendGeneral = new FormData();
+        formDataSendInstructions = new FormData();
     }
     function displayInfos() {
         console.log("show info");
         let effect = false;
         let outputInfos = document.querySelector("div#outputInfos");
         outputInfos.innerHTML = "";
-        let formData = new FormData(document.querySelector("form"));
-        for (let entry of formData) {
+        let formDataGeneral = new FormData(document.querySelector("form"));
+        for (let entry of formDataGeneral) {
             switch (entry[0]) {
                 case "Name":
                     if (entry[1] != "") {
                         outputInfos.innerHTML += "Name des Tranks: " + entry[1] + "<br>";
+                        formDataSendGeneral.append(entry[0], entry[1]);
                         console.log("case1");
                     }
                     break;
                 case "Beschreibung":
                     if (entry[1] != "") {
                         outputInfos.innerHTML += "Beschreibung: " + entry[1] + "<br>";
+                        formDataSendGeneral.append(entry[0], entry[1]);
                         console.log("case2");
                     }
                     break;
@@ -43,6 +83,7 @@ var L05_Hexenkessel_Client;
                     if (entry[1] != "-") {
                         outputInfos.innerHTML += "Trankwirkung: " + entry[1] + "<br>";
                         effect = true;
+                        formDataSendGeneral.append(entry[0], entry[1]);
                         console.log("case3");
                     }
                     break;
@@ -50,6 +91,7 @@ var L05_Hexenkessel_Client;
                     if (entry[1] != "0" && effect) {
                         outputInfos.innerHTML += "Dauer: " + entry[1] + "<br>";
                         console.log("case4");
+                        formDataSendGeneral.append(entry[0], entry[1]);
                         break;
                     }
                 default:
@@ -61,16 +103,18 @@ var L05_Hexenkessel_Client;
         let totalPrice = document.querySelector("#totalPrice");
         outputInstructions.innerHTML = "";
         totalPrice.innerHTML = "";
-        let formData2 = new FormData(document.querySelector("form"));
-        for (let entry of formData2) {
+        let formDataInstructions = new FormData(document.querySelector("formInstructions"));
+        for (let entry of formDataInstructions) {
             if (entry[0] == "Ingredienzen") {
                 let selector = "[value='" + entry[1] + "']";
                 let item = document.querySelector(selector);
                 let chosenAmount = entry[1] + "Menge";
-                let amount = Number(formData2.get(chosenAmount));
+                let amount = Number(formDataInstructions.get(chosenAmount));
                 let priceItem = Number(item.getAttribute("price"));
                 price += amount * priceItem;
                 outputInstructions.innerHTML += amount + " " + entry[1] + " hinzugeben." + "<br>";
+                formDataSendInstructions.append(entry[0], entry[1]);
+                formDataSendInstructions.append(chosenAmount, amount.toString());
             }
         }
         outputInstructions.innerHTML += "<br>";
@@ -79,8 +123,8 @@ var L05_Hexenkessel_Client;
     }
     function displayTemp() {
         let outputInstructions = document.querySelector("div#outputInstructions");
-        let formData2 = new FormData(document.querySelector("form"));
-        for (let entry of formData2) {
+        let formDataInstructions = new FormData(document.querySelector("formInstructions"));
+        for (let entry of formDataInstructions) {
             switch (entry[0]) {
                 case "Temperatur":
                     outputInstructions.innerHTML += "Grad: " + entry[1];
---
+++ Aufgabe5/server.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.L05_Hexenkessel_Client = void 0;
+const Http = require("http");
+const Url = require("url");
+var L05_Hexenkessel_Client;
+(function (L05_Hexenkessel_Client) {
+    let server = Http.createServer();
+    console.log(server);
+    let port = process.env.PORT;
+    if (port == undefined)
+        port = 50001;
+    console.log("Server starting on port:" + port);
+    server.listen(port);
+    server.addListener("request", handleRequest);
+    function handleRequest(_request, _response) {
+        console.log("What's up?");
+        _response.setHeader("content-type", "text/html; charset-utf-8");
+        _response.setHeader("Acces-Control-Allow-Origin", "*");
+        if (_request.url) {
+            let url = Url.parse(_request.url, true);
+            for (let key in url.query) {
+                _response.write(key + ":" + url.query[key] + "<br/>");
+            }
+            let jsonString = JSON.stringify(url.query);
+            _response.write(jsonString);
+        }
+        _response.write("This is my response");
+        _response.end();
+    }
+})(L05_Hexenkessel_Client = exports.L05_Hexenkessel_Client || (exports.L05_Hexenkessel_Client = {}));
+//# sourceMappingURL=server.js.map
